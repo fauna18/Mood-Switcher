2975efac804b217c6f68cd752f9af0e590ea2436
fauna18 <faunaedenellis@gmail.com>
2026-06-22 10:03:53 -0700

    Add files via upload

---
+++ python-text.py
@@ -0,0 +1,15 @@
+# a = [1, 2]
+# b = [3, 4]
+# a.append(b)
+# print(a)
+
+prices = {"apple": 2, "banana": 1}
+if 2 in prices.values:
+    print("found a price of 2")
+
+original = [1, 2, 3]
+backup = original
+backup.append(4)
+print(original)
+
+
