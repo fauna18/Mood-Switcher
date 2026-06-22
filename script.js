71b8f218de5d376f0452f3cc51df02d0951d3b74
fauna18 <faunaedenellis@gmail.com>
2026-06-22 10:06:43 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+function happy() {
+    document.getElementById("message").textContent = "Happy mode activated";
+    document.getElementById("page").style.background = "#e3d039";
+}
+
+function sad() {
+    document.getElementById("message").textContent = "Sad mode activated";
+    document.getElementById("page").style.background = "#0e14b0";
+}
+
+function jealous() {
+    document.getElementById("message").textContent = "Jealous mode activated";
+    document.getElementById("page").style.background = "#0f922e";
+}
+
+function angry() {
+    document.getElementById("message").textContent = "Angry mode activated";
+    document.getElementById("page").style.background = "#c11818";
+}
+
+function anxious() {
+    document.getElementById("message").textContent = "Anxious mode activated";
+    document.getElementById("page").style.background = "#e43b95";
+}
